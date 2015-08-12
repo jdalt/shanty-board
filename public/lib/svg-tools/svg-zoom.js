fac729c6210619bb13a03fcaae5383518df0e7c8
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-12 10:22:08 -0500

    update zoom lib and add points to svg

--- public/lib/svg-tools/svg-zoom.js
+++ public/lib/svg-tools/svg-zoom.js
@@ -2,26 +2,44 @@ angular.module('svg.zoom', [])
 .directive('svgZoom', [function() {
   return function($scope, element, attr) {
     var el = element[0];
-    if(el.tagName.toLowerCase() != 'svg') return;
+    var svg = el.viewportElement
 
+    // if(svg.tagName.toLowerCase() != 'svg') return;
+
+    // TODO: break out as service Helper method
+    function cursorPoint(evt) {
+      var pt = svg.createSVGPoint();
+      pt.x = evt.clientX; pt.y = evt.clientY;
+      return pt.matrixTransform(svg.getScreenCTM().inverse());
+    }
+
+    function setTransform(element, matrix) {
+      var s = 'matrix(' + matrix.a + ',' + matrix.b + ',' + matrix.c + ',' + matrix.d + ',' + matrix.e + ',' + matrix.f + ')';
+      element.setAttribute('transform', s);
+    }
+
+    var zoomLevel = 1
+    var repeat = .1
     function zoom(event) {
-      svgEl = event.currentTarget;
-
       var delta = event.deltaY; // check cross browser situation
-      var flipper = 1;
-      if(delta < 0) flipper = -1;
+      var oldZoomLevel = zoomLevel || 1
+      if(delta < 0) {
+        zoomLevel = zoomLevel * (1 - repeat)
+      } else {
+        zoomLevel *= (1 + repeat)
+      }
+      var zoomRelative = zoomLevel/oldZoomLevel
 
-      var viewBox = svgEl.viewBox.baseVal;
-      viewBox.height = viewBox.height + viewBox.height * .1 * flipper;
-      viewBox.width = viewBox.width + viewBox.width * .1 * flipper;
-      svgEl.setAttribute('viewBox', viewBoxStr(viewBox));
+      var point = svg.createSVGPoint()
+      point.x = event.clientX
+      point.y = event.clientY
+      var oldCTM = el.getCTM()
+        , relativePoint = point.matrixTransform(oldCTM.inverse())
+        , modifier = svg.createSVGMatrix().translate(relativePoint.x, relativePoint.y).scale(zoomRelative).translate(-relativePoint.x, -relativePoint.y)
+        , newCTM = oldCTM.multiply(modifier)
+      setTransform(el, newCTM)
     }
 
-    function viewBoxStr(vb) {
-      return vb.x +' '+ vb.y +' '+ vb.width +' '+ vb.height;
-    }
-
-    element.on('wheel', zoom);
+    svg.onmousewheel = zoom
   }
 }])
-
